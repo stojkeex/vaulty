44958af304f5cc60f369f5e50c00746cbf4ea8a9
stojkeex <stackflow.info@gmail.com>
2025-09-10 13:54:00 +0200

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html'),
-      },
-    },
-  },
 })
+
